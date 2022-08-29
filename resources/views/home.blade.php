8d34752b520f0a06a1b55c9e5bf139534c58e9ed
HannaFasya <hanna.firdausiah20@gmail.com>
2022-08-29 11:55:00 +0700

    new commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Belajar Blade | Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+@extends('layouts.main')
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-          <a class="navbar-brand" href="#">My Blog</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active"  href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/blog">Blog</a>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-    <div class="container mt-4">
-        <h1>Halaman Home</h1>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+@section('container')
+    <h1>Halaman Home</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function() {
     return view ('about', [
+        "title" => "About",
         "name" => "Hanna Fasya",
         "email" => "hanna.firdausiah20@gmail.com",
         "image" =>"hanna.jpg"
@@ -26,8 +29,10 @@ Route::get('/about', function() {
     ]);
 });
 
-Route::get('/blog',function(){
-    return view('posts');
+Route::get('/posts',function(){
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
 
 
